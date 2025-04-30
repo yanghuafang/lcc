7f5adbd16f292bc5ea30269750f1459b2f9be142
Yanghua Fang <yanghuafang@gmail.com>
2025-04-30 15:42:26 +0800

    Review build-env.sh to switch llvm version easily

--- scripts/build-env.sh
+++ scripts/build-env.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-export EXT_PATH="$EXT_PATH:/opt/homebrew/opt/flex/bin"
-export EXT_PATH="$EXT_PATH:/opt/homebrew/opt/bison/bin"
-export EXT_PATH="$EXT_PATH:/opt/homebrew/opt/llvm@14/bin"
+export LLVM_DIR=$(brew --prefix llvm@14)
+export FLEX_DIR=$(brew --prefix flex)
+export BISON_DIR=$(brew --prefix bison)
+
+export EXT_PATH="$EXT_PATH:$FLEX_DIR/bin"
+export EXT_PATH="$EXT_PATH:$BISON_DIR/bin"
+export EXT_PATH="$EXT_PATH:$LLVM_DIR/bin"
 export PATH="$EXT_PATH:$PATH"
 
-export EXT_CPPFLAGS="$EXT_CPPFLAGS -I/opt/homebrew/opt/flex/include"
-export EXT_CPPFLAGS="$EXT_CPPFLAGS -I/opt/homebrew/opt/llvm@14/include"
+export EXT_CPPFLAGS="$EXT_CPPFLAGS -I$FLEX_DIR/include"
+export EXT_CPPFLAGS="$EXT_CPPFLAGS -I$LLVM_DIR/include"
 export CPPFLAGS="$EXT_CPPFLAGS $CPPFLAGS"
 
-export EXT_LDFLAGS="$EXT_LDFLAGS -L/opt/homebrew/opt/flex/lib -L/opt/homebrew/opt/bison/lib"
-export EXT_LDFLAGS="$EXT_LDFLAGS -L/opt/homebrew/opt/llvm@14/lib/c++"
-export EXT_LDFLAGS="$EXT_LDFLAGS -Wl,-rpath,/opt/homebrew/opt/llvm@14/lib/c++"
-export EXT_LDFLAGS="$EXT_LDFLAGS -L/opt/homebrew/opt/llvm@14/lib"
+export EXT_LDFLAGS="$EXT_LDFLAGS -L$FLEX_DIR/lib"
+export EXT_LDFLAGS="$EXT_LDFLAGS -L$BISON_DIR/lib"
+export EXT_LDFLAGS="$EXT_LDFLAGS -L$LLVM_DIR/lib/c++"
+export EXT_LDFLAGS="$EXT_LDFLAGS -Wl,-rpath,$LLVM_DIR/lib/c++"
+export EXT_LDFLAGS="$EXT_LDFLAGS -L$LLVM_DIR/lib/unwind -lunwind"
+export EXT_LDFLAGS="$EXT_LDFLAGS -L$LLVM_DIR/lib"
 export LDFLAGS="$EXT_LDFLAGS $LDFLAGS"
